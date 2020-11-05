aa654aa039df6abdb33aa54a9f0b9b9d164c278e
loversix <903558256@qq.com>
2020-11-05 20:41:43 +0800

    买家商品

--- src/test/java/com/imooc/sell/service/impl/CategoryServiceImplTest.java
+++ src/test/java/com/imooc/sell/service/impl/CategoryServiceImplTest.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -24,13 +27,20 @@ public class CategoryServiceImplTest {
 
     @Test
     public void findAll() {
+        List<ProductCategory> productCategoryList = categoryService.findAll();
+        Assert.assertNotEquals(0, productCategoryList.size());
     }
 
     @Test
     public void findByCategoryTypeIn() {
+        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(Arrays.asList(1, 2, 3, 4));
+        Assert.assertNotEquals(0, productCategoryList.size());
     }
 
     @Test
     public void save() {
+        ProductCategory productCategory = new ProductCategory("男生专享", 10);
+        ProductCategory result = categoryService.save(productCategory);
+        Assert.assertNotNull(result);
     }
 }
